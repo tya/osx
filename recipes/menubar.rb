ca761cfbb990d0e90972bbcccb2ea00caca755c8
Alex Howells <alex@howells.me>
2013-06-29 19:01:15 -0700

    Icon sizes in Finder sidebar, highlight colors and more.
      * Be able to manipulate between small, medium and large sidebar icon sizing
      * Adjust the highlight color used globally. Seen when you select text, etc.
      * Adjust the visibility of scroll bars.
      * First pass at being able to manipulate Menu Bar items (e.g. Volume, Airport)

---
+++ recipes/menubar.rb
@@ -0,0 +1,27 @@
+#
+#  Cookbook Name: osx
+#  Recipe Name: menubar
+#
+#  Copyright 2013, Alex Howells <alex@howells.me>
+#  See the LICENSE file in the repository root for more information.
+#
+
+include_recipe "osx::common"
+
+# We allow attributes to be specified in short form ("Volume").
+# That then needs to be expanded to a full path before being passed to osx_userdefaults.
+extras_exploded = Array.new
+
+node['osx']['settings']['menubar']['extras'].each do |item|
+  extras_exploded.push("\"/System/Library/CoreServices/Menu Extras/#{item}.menu\"")
+end
+
+osx_userdefaults "Menu Bar :: Determine which Apple items will appear in the Menu Bar" do
+  domain "com.apple.systemuiserver"
+  key "menuExtras"
+  type "array"
+  value extras_exploded.each { |item| print "#{item.to_s} " }
+  notifies :run, 'execute[killall SystemUIServer]', :delayed
+end
+
+
