7a2b322745b78cc9c8cacbcf060e9b1003adb032
Alex Howells <agh@users.noreply.github.com>
2014-10-29 04:43:33 +0000

    Add a name attribute to the metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,7 @@
+name             "osx"
 maintainer       "Alex Howells"
 maintainer_email "alex@howells.me"
 license          "Apache 2.0"
 description      "Manage OSX application and system settings"
-version          "2.4.2"
+version          "2.4.3"
 supports         "mac_os_x"
