66fcb2c3f5cd377402bf8c840e8b8296905bbf5d
Alex Howells <alex@howells.me>
2014-11-02 00:02:11 +0000

    Stub out new recipes to follow Real Soon Now (:tm:)

---
+++ recipes/gatekeeper.rb
@@ -0,0 +1,10 @@
+#
+#  Cookbook Name: osx
+#  Recipe Name: gatekeeper
+#
+#  Copyright 2014, Aeode Ltd <hello@aeode.com>
+#  See the LICENSE file in the repository root for more information.
+#
+
+# ToDo: Look at `spctl` with not_if qualifier. Set state via attriute.
+# To Disable: `spctl --master-disable`
---
+++ recipes/hostname.rb
@@ -0,0 +1,9 @@
+#
+#  Cookbook Name: osx
+#  Recipe Name: hostname
+#
+#  Copyright 2014, Aeode Ltd <hello@aeode.com>
+#  See the LICENSE file in the repository root for more information.
+#
+
+# ToDo: Support configuring hostname of Mac system using attribute.
